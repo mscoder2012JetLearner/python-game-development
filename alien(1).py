ede8aec3cd561a52a425e58e4bd304f9ea0d7e70
mscoder2012JetLearner <maxschwarz005@gmail.com>
2025-09-09 18:36:21 +0100

    Add files via upload

--- alien(1).py
+++ alien(1).py
@@ -1,5 +1,6 @@
 import pgzrun
 import random
+import time
 
 HEIGHT=500
 WIDTH=500
@@ -10,7 +11,25 @@ messege="hi alien"
 def draw():
     screen.fill("gold")
     alien.draw()
-    screen.draw.text(messege,center=(250,250),fontsize=(100))
+    screen.draw.text(messege,center=(250,250),fontsize=(50))
+
+def on_mouse_down(pos):
+    global messege
+    if alien.collidepoint(pos):
+       messege="good shot"
+       alien.x=random.randint(0,450)
+       alien.y=random.randint(0,450)
+
+    else:
+        messege="you missed"
+        
+
+
+
+    
+
+
+
 
 
 
