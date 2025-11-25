08beeacd67fde5974f5f5f0bc699b859035752e3
mscoder2012JetLearner <maxschwarz005@gmail.com>
2025-11-25 18:55:13 +0000

    Add files via upload

--- quiz_game.py
+++ quiz_game.py
@@ -1,7 +1,9 @@
 import random
 import pgzrun
+import os
+os.environ['SDL_VIDEO_CENTERED']='1'
 
-HEIGHT=650
+HEIGHT=670
 WIDTH=870
 
 TITLE="quiz game"
@@ -18,11 +20,31 @@ answer_box3.move_ip(20,450)
 answer_box4=Rect(0,0,300,150)
 answer_box4.move_ip(350,450)
 
+question_box=Rect(0,0,630,100)
+question_box.move_ip(20,150)
+
+timer_box=Rect(0,0,100,100)
+timer_box.move_ip(675,150)
+
+skip_box=Rect(0,0,100,330)
+skip_box.move_ip(675,270)
+
+intro_box=Rect(0,0,755,100)
+intro_box.move_ip(20,20)
+
+
+
 def draw():
     screen.draw.filled_rect(answer_box1,"red")
     screen.draw.filled_rect(answer_box2,"red")
     screen.draw.filled_rect(answer_box3,"red")
     screen.draw.filled_rect(answer_box4,"red")
+    screen.draw.filled_rect(question_box,"magenta")
+    screen.draw.filled_rect(timer_box,"blue")
+    screen.draw.filled_rect(skip_box,"dark green")
+    screen.draw.filled_rect(intro_box,"mediumvioletred")
+    screen.draw.textbox("Welcome to Quizz master!",intro_box,color="white")
+
    
 
 
